562502f18c5f3f5a6ce4939c0db31d8f0debd529
bardia-mhd <bardia.mohammadi@yahoo.com>
2023-07-15 09:27:25 +0330

    Change main structure with task_manager

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 def main():
-    todo_list = load_todo_list()
-
-    print("To-Do List Manager!")
-
-    display_todo_list(todo_list)
     
-    while True:
-        user_input = input("Enter a command: ")
+    task_manager = TaskManager()
 
-        if user_input == "add":
-            add_task(todo_list)
-        elif user_input == "delete":
-            delete_task(todo_list)
-        elif user_input == "complete":
-            complete_task(todo_list)
-        elif user_input == "display":
-            display_todo_list(todo_list)
-        elif user_input == "save":
-            save_todo_list(todo_list)
-        elif user_input == "quit":
-            break
-        else:
-            print("Invalid command. Please try again.")
-
-    print("Exit!")
+if __name__ == "__main__":
+    main()
